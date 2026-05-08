5e32d9c7ccdf13e0c7038a957360d0cb97a42d49
Harshita Choudhary <Kumariharshita258@gmail.com>
2026-05-08 22:13:30 +0530

    Work Section - Update

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -4,10 +4,10 @@ import profile from '../../assets/profile.png'
 const HeroSection = () => {
   return (
     <div className='min-h-[60vh] flex pt-20 md:pt-40 pb-20 [text-shadow:0_0_15px_#f64b35]'>
-        <p className='text-white font-quote text-4xl md:text-5xl lg:text-6xl text-center leading-relaxed md:leading-snug'>
+        <p className='px-10 text-white font-quote text-4xl md:text-5xl lg:text-6xl text-center leading-relaxed md:leading-snug'>
           Just a girl in a world full of chaos… <br className="hidden md:block" /> quietly building her own path.
         </p>
-        <img className='w-72 h-72 absolute right-45 top-50 rounded-tr-3xl rounded-bl-3xl border-4 border-primary shadow-[0_0_15px_#f0f7f99c]' src={profile} alt="Profile Photo" />
+        <img className='w-82 h-82 absolute right-45 top-50 rounded-tr-3xl rounded-bl-3xl border-4 border-primary shadow-[0_0_15px_#f0f7f99c]' src={profile} alt="Profile Photo" />
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
-            <div className="navbar h-16 flex justify-between items-center backdrop-blur-sm bg-white/10 py-5 px-6 md:px-15 rounded-lg [text-shadow:0_0_10px_#f64b35]">
+            <div className="navbar h-16 flex justify-between items-center py-5 px-6 md:px-15 rounded-lg [text-shadow:0_0_10px_#f64b35]">
                 <div className="left flex items-center text-white font-bold text-xl md:text-2xl">
                     <div className="w-12 h-12 mr-4 rounded-full border border-purple-300/60 shadow-[0_0_20px_rgba(168,85,247,0.4)] flex items-center justify-center text-purple-300 font-quote text-3xl bg-purple-900/30 backdrop-blur-md">
                       H
